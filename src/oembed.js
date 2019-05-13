5908f670882aae82dc0d9af7a39ba24757a356f8
Rune Botten <rbotten@gmail.com>
2019-05-13 15:15:08 +0200

    Drop query-string dependency and pass User-Agent

--- src/oembed.js
+++ src/oembed.js
@@ -1,21 +1,25 @@
 const getEndpoint = require('./getEndpoint')
 const rp = require('request-promise')
-const queryString = require('query-string')
 
-const oembed = (url, options = {}) => {
+const oembed = (url, options = { format: 'json' }) => {
   const endpoint = getEndpoint(url)
 
   if (!endpoint) {
     return Promise.resolve(null)
   }
 
-  const params = Object.assign({
-    url,
-    format: 'json'
-  }, options)
+  const params = Object.assign(options, {
+    url
+  })
 
-  const endpointUrl = `${endpoint.url}?${queryString.stringify(params)}`
-  return rp(endpointUrl).then(result => JSON.parse(result))
+  return rp({
+    uri: endpoint.url,
+    qs: params,
+    headers: {
+      'User-Agent': 'resolve-oembed'
+    },
+    json: params.format === 'json'
+  })
 }
 
 module.exports = oembed
